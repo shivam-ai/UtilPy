24ac62bd5723163414199dd9b061ef7b25143994
Shivam Agrawal <Shivam.Agrawal@lntinfotech.com>
2018-08-30 15:39:10 +0530

    updated text to speech

--- text2speech.py
+++ text2speech.py
@@ -1,9 +1,31 @@
 ## ! pip install pyttsx3
-import pyttsx3
+## ! pip install gTTS
 import os
 import sys
+import pyttsx3
+from gtts import gTTS
 
-engine= pyttsx3.init()
+def pytts(text):
+    engine= pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
 
-engine.say("Hello Shivam, kya haal chal hai?")
-engine.runAndWait()
+def gtt(text):
+    tts= gTTS(text= text, lang='en')
+    tts.save('shivam_say.mp3')
+    os.system('shivam_say.mp3')
+
+
+
+
+
+
+
+
+
+
+
+if __name__=='__main__':
+    text= "Hello Shivam, kya haal chal hai"
+    pytts(text)
+    gtt(text)
